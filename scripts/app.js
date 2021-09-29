5586ad8d3ecf85918e9cb3c67aaaba736722c8a4
hollylouisarose <hlrstratton@gmail.com>
2021-09-29 17:10:04 +0100

    styles added and javascript fade in animation added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-console.log('hello testing')
+const animatedTags = document.querySelectorAll('section')
+
+animatedTags.forEach(tag => {
+  tag.style.opacity = 0
+})
+
+const handleFadeIn = () => {
+
+  let delay = 0.25
+
+  animatedTags.forEach(tag => {
+    const tagTop = tag.getBoundingClientRect().top
+    const tagBottom = tag.getBoundingClientRect().bottom
+
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      tag.style.animation = `fadein 1s ${delay}s both`
+      delay = delay + 0.25
+    } else {
+      tag.style.opacity = 0
+      tag.style.animation = ''
+    }
+
+  })
+
+}
+
+document.addEventListener('scroll', handleFadeIn)
