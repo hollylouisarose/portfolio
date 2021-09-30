7802b9edc3582600c448987eb7a3c05d569a8505
hollylouisarose <hlrstratton@gmail.com>
2021-09-30 17:37:06 +0100

    initial darkmode added

---
+++ scripts/app.js
@@ -0,0 +1,55 @@
+const animatedTags = document.querySelectorAll('section')
+const body = document.querySelector('body')
+const progressBar = document.querySelector('div.progress-bar')
+const button = document.querySelector('#toggle')
+
+body.classList.remove('dark-theme')
+
+animatedTags.forEach(tag => {
+  tag.style.opacity = 0
+})
+
+const handleFadeIn = () => {
+
+  let delay = 0.25
+
+  animatedTags.forEach(tag => {
+    const tagTop = tag.getBoundingClientRect().top
+    const tagBottom = tag.getBoundingClientRect().bottom
+
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      tag.style.animation = `fadein 1s ${delay}s both`
+      delay = delay + 0.25
+    } else {
+      tag.style.opacity = 0
+      tag.style.animation = ''
+    }
+
+  })
+
+}
+
+const handleScroll = () => {
+
+  const pixels = window.pageYOffset
+  const pageHeight = body.getBoundingClientRect().height
+  const totalScrollDistance = pageHeight - window.innerHeight
+  const percentage = pixels / totalScrollDistance
+  
+  progressBar.style.width = `${100 * percentage}%`
+
+}
+
+const handleColourTheme = () => {
+
+  body.classList.toggle('dark-theme')
+}
+
+console.log(progressBar.classList)
+
+
+handleFadeIn()
+
+document.addEventListener('scroll', handleFadeIn)
+document.addEventListener('scroll', handleScroll)
+button.addEventListener('click', handleColourTheme)
